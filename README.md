37d322231cfb24feebb8487d1d3ccca9a5df5e8c
HistoireTheOracle <peter5th1234@gmail.com>
2022-06-24 18:28:51 -0500

    Committed All Local Changes to Rushify
    Added Ritual Synchro Xyz and Dark Synchro as well as respective level types
    
    (test run on github)

---
+++ README.md
@@ -0,0 +1,29 @@
+Changelog:
+
+2022/06/03:
+- Fixed an issue where card text is not displayed correctly the first time they are loaded.
+
+2022/01/14:
+- Fixed an issue where out of range Level generates invalid images
+- Fixed an issue where it is possible to have negative ATK/DEF values
+- Removed redundant codes
+- Removed the need to fill out irrelevant fields in template
+- Various bugfixes
+
+2022/01/10:
+- Fixed a bug where Field Spells are not properly generated from batch create
+- Added banner and new name
+- Various bugfixes
+
+2022/01/09:
+- Implemented batch create
+- Fixed a bug where Maximum ATK is not displayed properly for Rush Rare
+
+2022/01/06:
+- Added field spell option
+
+2022/01/05:
+- Initial release
+
+Rushify is a lightweight card maker for Rush Duel cards with batch create feature. 
+I do not own the images. Yu-Gi-Oh! and Yu-Gi-Oh! Rush Duel are properties of Konami.
---
+++ js/main.js
@@ -0,0 +1,262 @@
+$(document).ready(function(){
+
+	$('#card-text').trumbowyg();
+
+    var monsterOnly = $('.monster-only');
+    var stOnly = $('.st-only');
+    var effOnly = $('.effect-monster-only');
+    var normOnly = $('.normal-monster-only');
+    var allInputs = $('.input');
+    var legend = $('.card-legend');
+    var cardAttribute = $('.card-attribute')
+    var maximumOnly = $('.maximum-only');
+    $('.progress').hide();
+    $('#progress-text').hide();
+
+    let cardTypeSelected = $('#ctype option:selected').val();
+    if (cardTypeSelected == 'Normal'){
+        $('.lore').css('font-family', 'CardLore');
+    } else{
+        $('.lore').css('font-family', 'CardEffect');
+    }
+
+    $('.card-st-type').hide();
+    maximumOnly.hide();
+    stOnly.hide();
+    effOnly.hide();
+    squeezeToFit($('.card-name-1'), $('.card-name'));
+    resizeToFit($('.lore'), $('.card-lore'));
+
+    $('.input').change(function(){
+        let cardName = $('#card-name').val();
+        //Change the card name
+        if (cardName != ""){
+            $('.card-name-1').html(cardName);
+            squeezeToFit($('.card-name-1'), $('.card-name'));
+        }
+        //Show monster card or spell/trap card info input depending on player selection
+        let cardTypeSelected = $('#ctype option:selected').val();
+        if (cardTypeSelected == "Normal" || cardTypeSelected == "Effect" || cardTypeSelected == "Fusion" || cardTypeSelected == "Synchro" || cardTypeSelected == "Ritual" || cardTypeSelected == "Dark-Synchro" || cardTypeSelected == "Xyz"){
+            stOnly.hide();
+            monsterOnly.show();
+        } else{
+            stOnly.show();
+            monsterOnly.hide();
+        }
+        //Change the card frame
+        $('.card-frame').attr('src', 'view/img/frame/' + cardTypeSelected + '.png');
+        //Change the attribute
+        if (cardTypeSelected == "Spell"){
+            cardAttribute.attr('src', 'view/img/attribute/SPELL.png');
+        } else if (cardTypeSelected == "Trap"){
+            cardAttribute.attr('src', 'view/img/attribute/TRAP.png');
+        } else{
+            cardAttribute.attr('src', 'view/img/attribute/' + $('#monster-attribute option:selected').val() + '.png');
+        }
+        //Change the monster type
+        let monsterType = $('#monster-type').val();
+        if (cardTypeSelected == "Spell"){
+            $('.card-type-1').html("Spell Card");
+        } else if (cardTypeSelected == "Trap"){
+            $('.card-type-1').html("Trap Card");
+        } else{
+            $('.card-type-1').html(monsterType);
+        }
+        //Change the monster level
+        if (cardTypeSelected != "Xyz" || cardTypeSelected != "Dark-Synchro"){
+            let level = $('#level').val() > 13 ? 13 : ($('#level').val() < 0 ? 0 : $('#level').val());
+            $('.card-level').attr('src', 'view/img/stat/Level-' + level + '.png');
+        } else if (cardTypeSelected == "Xyz"){
+            let level = $('#level').val() > 13 ? 13 : ($('#level').val() < 0 ? 0 : $('#level').val());
+            $('.card-level').attr('src', 'view/img/stat/Rank-' + level + '.png');
+        } else if (cardTypeSelected == "Dark Synchro"){
+            let level = $('#level').val() > 13 ? 13 : ($('#level').val() < 0 ? 0 : $('#level').val());
+            $('.card-level').attr('src', 'view/img/stat/NegLevel-' + level + '.png');
+        }
+        //Change the atk/def
+        if (cardTypeSelected != "Spell" && cardTypeSelected != "Trap"){
+            $('.card-atk').html($('#atk').val() < 0 ? 0 : $('#atk').val());
+            $('.card-def').html($('#def').val() < 0 ? 0 : $('#def').val());
+            $('.card-atk-rush').html($('#atk').val() < 0 ? 0 : $('#atk').val());
+            $('.card-def-rush').html($('#def').val() < 0 ? 0 : $('#def').val());
+            if ($('#maximum').is(':checked')){
+                $('.card-max-atk').html($('#maximum-atk').val() < 0 ? 0 : $('#maximum-atk').val());
+                $('.card-max-atk-rush').html($('#maximum-atk').val() < 0 ? 0 : $('#maximum-atk').val());
+            }
+        }
+        //Enable Maximum ATK input
+        if (cardTypeSelected != "Spell" && cardTypeSelected != "Trap" && $('#maximum').is(":checked")){
+            $('#maximum-atk').removeAttr('disabled');
+            $('.maximum-only').show();
+        } else{
+            $('#maximum-atk').attr('disabled', 'disabled');
+            $('.maximum-only').hide();
+        }
+        //Change rarities
+        let rarity = $('#rarity option:selected').val();
+        if (rarity == "common" && (cardTypeSelected != "Xyz" && cardTypeSelected != "Dark-Synchro")){
+            $('.card-name-1').removeClass('rarity-rush');
+            $('.card-name-1').removeClass('rarity-whitename');
+            $('.card-atk').removeClass('rarity-rush');
+            $('.card-def').removeClass('rarity-rush');
+            $('.card-max-atk').removeClass('rarity-rush');
+        } else if (rarity == "common" && (cardTypeSelected == "Xyz" || cardTypeSelected == "Dark-Synchro")){
+            $('.card-name-1').removeClass('rarity-rush');
+            $('.card-name-1').addClass('rarity-whitename');
+            $('.card-atk').removeClass('rarity-rush');
+            $('.card-def').removeClass('rarity-rush');
+            $('.card-max-atk').removeClass('rarity-rush');
+        } else{
+            }
+            if (!$('.card-name-1').hasClass('rarity-rush')){
+                $('.card-name-1').addClass('rarity-rush');
+            }
+            if (!$('.card-atk').hasClass('rarity-rush')){
+                $('.card-atk').addClass('rarity-rush');
+            }
+            if (!$('.card-def').hasClass('rarity-rush')){
+                $('.card-def').addClass('rarity-rush');
+            }
+            if (!$('.card-max-atk').hasClass('rarity-rush')){
+                $('.card-max-atk').addClass('rarity-rush');
+            }
+        },
+
+        //Change card creator
+        $('.card-creator').html($('#creator').val());
+
+        //Change card set number
+        $('.card-set-number').html($('#set-number').val());
+
+        if (cardTypeSelected == 'Spell' || cardTypeSelected == 'Trap'){
+            if ($('#st-type option:selected').val() != 'Normal') {
+                if (cardTypeSelected == 'Spell'){
+                    $('.card-type-1').addClass('p40');
+                } else{
+                    $('.card-type-1').addClass('p50');
+                }
+                $('.card-st-type').show();
+                $('.card-st-type').attr('src', 'view/img/type/' + $('#st-type option:selected').val() + '.png');
+            } else{
+                $('.card-st-type').hide();
+                $('.card-type-1').removeClass('p40');
+                $('.card-type-1').removeClass('p50');
+            }
+        } else{
+            $('.card-st-type').hide();
+            $('.card-type-1').removeClass('p40');
+            $('.card-type-1').removeClass('p50');
+        }
+        if (cardTypeSelected == 'Normal'){
+            $('.lore').css('font-family', 'CardLore');
+        } else{
+            $('.lore').css('font-family', 'CardEffect');
+        }
+    });
+    //Change card image
+    $('#image-url').change(function(){
+        $(".card-image").attr("src", $("#image-url").val());
+    });
+    $('.trumbowyg-editor').keyup(function(){
+        //Change card text
+        $('.lore').html($('.trumbowyg-editor').html());
+        resizeToFit($('.lore'), $('.card-lore'));
+    });
+    $('.trumbowyg-editor').focusout(function(){
+        //Change card text
+        $('.lore').html($('.trumbowyg-editor').html());
+        resizeToFit($('.lore'), $('.card-lore'));
+    });
+    //Show or hide the LEGEND icon
+    $('#legend').change(function(){
+        if ($('#legend').is(":checked")){
+            legend.show();
+        } else{
+            legend.hide();
+        }
+    });
+    $('#upload-img').on('change', function () {
+    	var form = new FormData();
+    	form.append("image", $('#upload-img').prop('files')[0])
+    	var settings = {
+    	  "url": "https://api.imgbb.com/1/upload?key=e44acc4c48ba88413eda3a867905113c",
+    	  "method": "POST",
+    	  "timeout": 0,
+    	  "processData": false,
+    	  "mimeType": "multipart/form-data",
+    	  "contentType": false,
+    	  "data": form
+    	};
+
+    	$.ajax(settings).done(function (response) {
+      	var jx = JSON.parse(response);
+      	$("#image-url").val(jx.data.url);
+    	$(".card-image").attr("src", $("#image-url").val());
+      });
+    });
+    $("#upload-btn").click(function(){
+    	$("#upload-img").click();
+    });
+    $("#upload-file-btn").click(function(){
+    	$("#upload-file").click();
+    });
+    $('#random-generate').click(function(){
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < 4; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        result += "-";
+        for ( var i = 0; i < 5; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        $('#set-number').val(result);
+        //Change card set number
+        $('.card-set-number').html(result);
+    });
+    $('#save').click(function(){
+        let div = $('#display-card')[0];
+        html2canvas(div, {allowTaint:true, useCORS:true}).then(
+        function (canvas) {
+            $('#canvas').html(canvas);
+            saveAs(canvas.toDataURL(), $('#set-number').val() + '.png');
+        })
+    });
+
+    function saveAs(uri, filename){
+        var link = document.createElement('a');
+        if (typeof link.download === 'string') {
+            link.href = uri;
+            link.download = filename;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } else {
+            window.open(uri);
+        }
+    }
+
+    function squeezeToFit(text, div){
+        var larger = text.width() > div.width();
+        if (larger){
+            scale = div.width() / text.width();
+        } else{
+            scale = 1;
+        }
+        text.css('transform', 'scale(' + scale + ', 1)');
+        text.css('transform-origin', 'top left');
+    }
+
+    function resizeToFit(text, div){
+        var larger = text.height() > div.height();
+        if (larger){
+            textFit(div);
+            text.css('font-size', 'auto');
+        } else{
+            textFit(div);
+            text.css('font-size', '100%');
+        }
+    }
+
+});
